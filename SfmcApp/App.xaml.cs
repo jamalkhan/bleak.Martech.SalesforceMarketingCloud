1727d8f6daa66d074d7c14a117d353f75c99e2fd
Jamal Khan <jamal.h.khan@gmail.com>
2025-04-29 17:04:42 -0400

    yup. i'm that guy. I've never built a mac app, but i'm building a macapp for SFMC

---
+++ SfmcApp/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace SfmcApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		//MainPage = new AppShell();
+	}
+
+	protected override Window CreateWindow(IActivationState activationState)
+    {
+        // Create and return the main window with the root page
+        return new Window(new MainPage());
+    }
+}
---
+++ SfmcApp/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.ObjectModel;
+
+namespace SfmcApp;
+
+public partial class MainPage : ContentPage
+{
+	public int count = 0;
+
+	public ObservableCollection<TreeNode> Nodes { get; set; }
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		Nodes = new ObservableCollection<TreeNode>
+        {
+            new TreeNode
+            {
+                Name = "Parent A",
+                Children = new List<TreeNode>
+                {
+                    new TreeNode { Name = "Child A1" },
+                    new TreeNode { Name = "Child A2" }
+                }
+            },
+            new TreeNode
+            {
+                Name = "Parent B",
+                Children = new List<TreeNode>
+                {
+                    new TreeNode { Name = "Child B1" }
+                }
+            }
+        };
+
+        BindingContext = this;
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
